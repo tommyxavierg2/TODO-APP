5ea5916a73d1b1b775ddd6835b57991e82d1a606
tommyxavierg2 <tommyxavierg@gmail.com>
2017-11-04 03:12:26 -0400

    A complete TODO APP, tested on android device and ready to be reviewed.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,52 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  icons: string[];
+  items: Array<{description: string, isCompleted: boolean}>;
+  newItem: {description: string, isCompleted: boolean};
 
   constructor(public navCtrl: NavController) {
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
 
+    this.items = [];
+    this.newItem = {
+      description: "",
+      isCompleted: false
+    };
+  }
+
+  addNewItem() {
+    this.items.push(
+      {
+        description: this.newItem.description,
+        isCompleted: false
+      }
+    );
+
+    this.newItem.description = "";
+  }
+
+  markAsCompleted(taskIndex) {
+    if (!this.items[taskIndex].isCompleted) {
+        this.items[taskIndex].isCompleted = !this.items[taskIndex].isCompleted;
+    } else {
+           this.items[taskIndex].isCompleted = !this.items[taskIndex].isCompleted;
+            }
+
+        //console.log(`CURRENT TASK ${taskIndex}`);
+  }
+
+  delItem(index) {
+    this.items.splice(index, 1);
+  }
+
+  isCompleted = function(arr) {
+    return arr.filter(task => task.isCompleted == true).length;
+   }
+
+  inProgress = function(arr) {
+    return arr.filter(task => task.isCompleted == false).length;
   }
 
 }
